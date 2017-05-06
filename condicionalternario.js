071f05bb58c794771108dd08ecbc69d7ea6356f6
Tatiana Ruiz <tatianaruizparedes@gmail.com>
2017-05-06 12:57:21 -0500

    construyendo funciones dentro del condicional

--- condicionalternario.js
+++ condicionalternario.js
@@ -2,5 +2,11 @@ var isArthuro=true;
 var isking=true;
 
 
- isArthuro && isking? alert ("el rey es Arturo"): alert ("el rey no es Arturo");
+ isArthuro && isking? function (){
+  alert ("Es rey");
 
+console.log ("su arma es Excalibur")
+}():
+function(){
+	alert ("este no es el rey");
+console.log ("denle un cuchillo de cocina")}();
